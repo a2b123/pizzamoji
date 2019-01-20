a561d0427f3c3f240d85787aeb599af70f47ae0d
a2b123 <atbhati2@gmail.com>
2019-01-20 01:45:39 -0600

    ContactUsViewController

---
+++ PizzaMoji/Controllers/ContactUsViewController.h
@@ -0,0 +1,17 @@
+//
+//  ContactUsViewController.h
+//  PizzaMoji
+//
+//  Created by Amar Bhatia on 1/20/19.
+//  Copyright © 2019 Amar-Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactUsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ PizzaMoji/Controllers/ContactUsViewController.m
@@ -0,0 +1,25 @@
+//
+//  ContactUsViewController.m
+//  PizzaMoji
+//
+//  Created by Amar Bhatia on 1/20/19.
+//  Copyright © 2019 Amar-Bhatia. All rights reserved.
+//
+
+#import "ContactUsViewController.h"
+
+@interface ContactUsViewController ()
+
+@end
+
+@implementation ContactUsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.navigationItem.title = @"Contact Us";
+
+}
+
+@end
