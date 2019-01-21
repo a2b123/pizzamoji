f5353a244f09273e5986b891df2bede9361ba4ec
a2b123 <atbhati2@gmail.com>
2019-01-21 01:26:56 -0600

    Added IBOutlets to KeyboardViewController and added label to ContactUsViewController

--- PizzaMojiKeyboard/KeyboardViewController.m
+++ PizzaMojiKeyboard/KeyboardViewController.m
@@ -10,6 +10,35 @@
 
 @interface KeyboardViewController ()
 
+
+@property UICollectionView *collectionView;
+@property UICollectionView *letterKeyboardCollectionView;
+@property NSArray *imageNames;
+
+
+@property (weak, nonatomic) IBOutlet UIButton *globeKey;
+
+// Keyboard Rows
+@property (weak, nonatomic) IBOutlet UIView *numbersRow1View;
+@property (weak, nonatomic) IBOutlet UIView *numbersRow2View;
+@property (weak, nonatomic) IBOutlet UIView *numbersSymbolsRow3View;
+
+// Keys
+@property (weak, nonatomic) IBOutlet UIView *symbolsRow1View;
+@property (weak, nonatomic) IBOutlet UIView *symbolsRow2View;
+
+@property (weak, nonatomic) IBOutlet UIButton *returnButton;
+@property (weak, nonatomic) IBOutlet UIButton *spaceButton;
+@property (weak, nonatomic) IBOutlet UIButton *shiftButton;
+
+@property (weak, nonatomic) IBOutlet UIButton *oneTwoThreeButton;
+@property (weak, nonatomic) IBOutlet UIButton *switchModeRow4Button;
+@property (weak, nonatomic) IBOutlet UIButton *switchModeRow3Button;
+
+@property (weak, nonatomic) IBOutlet UIButton *letterButtonsArray;
+
+
+
 @end
 
 @implementation KeyboardViewController
@@ -17,6 +46,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    NSURL *localDirectory = [[NSFileManager defaultManager] URLsForDirectory:NSLibraryDirectory inDomains:NSUserDomainMask][0];
+    NSLog(@"KEYBOARD ---> %@", localDirectory.absoluteString);
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(onKeyboardHide:) name:UIKeyboardWillHideNotification object:nil];
+    
+    
+
 }
 
 
